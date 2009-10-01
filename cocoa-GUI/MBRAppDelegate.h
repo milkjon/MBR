3ee0d667c1c77815a8e9967cbe14613e51837f1c
Jonathan Milk <milkjon@gmail.com>
2009-10-01 03:52:42 +0300

    renamed tunes array to requests

--- cocoa-GUI/MBRAppDelegate.h
+++ cocoa-GUI/MBRAppDelegate.h
@@ -8,7 +8,7 @@
 #import <Cocoa/Cocoa.h>
 
 @interface MBRAppDelegate : NSObject {
-	NSMutableArray *tunes;
+	NSMutableArray *requests;
 	NSTimer *timer_;
 	
 	IBOutlet NSArrayController *arrayController;
