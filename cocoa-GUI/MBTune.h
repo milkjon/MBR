b8dc8db0eea188a42c11dfca4152dd1a13a8dde5
Jonathan Milk <milkjon@gmail.com>
2009-10-01 03:35:20 +0300

    Changes to the model class

--- cocoa-GUI/MBTune.h
+++ cocoa-GUI/MBTune.h
@@ -9,32 +9,31 @@
 
 
 @interface MBTune : NSObject {
-	NSString* artist_;
-	NSString* title_;
-	NSString* trackID_;
-	NSString* length_;
-	NSString* path_;
+	NSString *artist_;
+	NSString *title_;
+	NSString *album_;
+	NSString *trackID_;
+	NSString *length_;
 	
 	NSDate* requestTime_;
 	NSString* requester_;
 	NSString* dedication_;
 }
 
--(NSString*) artist;
--(NSString*) path;
--(NSString*) title;
--(NSString*) requester;
--(NSString*) dedication;
--(NSString*) trackID;
--(NSString*) length;
--(NSDate*) requestTime;
+-(NSString *) artist;
+-(NSString *) title;
+-(NSString *) album;
+-(NSString *) requester;
+-(NSString *) dedication;
+-(NSString *) trackID;
+-(NSDate *) requestTime;
 
--(void) setArtist: (NSString*) artist;
--(void) setPath: (NSString*) path;
--(void) setTitle: (NSString*) title;
--(void) setRequester: (NSString*) requester;
--(void) setDedication: (NSString*) dedication;
--(void) setTrackID: (NSString*) trackID;
--(void) setLength: (NSString*) length;
--(void) setRequestTime: (NSDate*) requestTime;
+-(void) setArtist: (NSString *) artist;
+-(void) setTitle: (NSString *) title;
+-(void) setAlbum: (NSString *) album;
+-(void) setRequester: (NSString *) requester;
+-(void) setDedication: (NSString *) dedication;
+-(void) setTrackID: (NSString *) trackID;
+-(void) setLength: (NSString *) length;
+-(void) setRequestTime: (NSDate *) requestTime;
 @end
