bfaca4fa147bc736471823e54fc8b15205f34e12
Jonathan Milk <milkjon@gmail.com>
2009-10-07 01:24:24 +0300

    Using persistent ID's everywhere

--- cocoa-GUI/MBRAppDelegate.m
+++ cocoa-GUI/MBRAppDelegate.m
@@ -21,14 +21,14 @@
 	
 	// Create two dummy objects and add them to the list
 	MBTune *tune1 = [[[MBTune alloc] init] autorelease];
-	[tune1 setTrackID: @"23746"];
+	[tune1 setTrackID: @"F36B634931C2E9A0"];
 	[tune1 setTitle: @"When Summer Comes"];
 	[tune1 setArtist: @"Pepper"];
 	[tune1 setLength: @"3:16"];
 	[tune1 setRequester: @"John"];
 
 	MBTune *tune2 = [[[MBTune alloc] init] autorelease];
-	[tune2 setTrackID: @"9400"];
+	[tune2 setTrackID: @"57802A1965D11511"];
 	[tune2 setTitle: @"Parachute"];
 	[tune2 setArtist: @"Sean Lennon"];
 	[tune2 setLength: @"3:19"];
@@ -79,7 +79,7 @@
 	NSString *source = [NSString stringWithFormat: 
 		@"tell application \"iTunes\"\n"
 		@"    set pl to some playlist whose name is \"Library\"\n"
-		@"    set t to some track of pl whose database ID is %@ \n"
+		@"    set t to some track of pl whose persistent ID is \"%@\" \n"
 		@"    set target to some playlist whose name is \"%@\" \n"
 		@"    add (get location of t) to target \n"
 		@"end tell ", trackID, playlist]; 
